af5c7c2b27c927029b69df05243289ebe04ca178
slmarshall1 <slmarshall1@uclan.ac.uk>
2017-02-26 12:50:50 +0000

    Example JSON code
    I have created this code to test out how JSON and Ajax work.  The plan is to pull the data from this file for testing purposes.

---
+++ jsonExample.js
@@ -0,0 +1,19 @@
+[
+
+{"name":"gerry",
+	"species":"Cat",
+	"favFood":"whiskas"
+},
+
+{
+	"name":"daniel",
+	"species":"dog",
+	"favFood":"chappie"
+}
+
+{
+	"name":"joey",
+	"species":"pony",
+	"favFood":"polos"
+}
+]
