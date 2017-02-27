d33a7cf1f98a62b6d0df7dc70427ed34bec431b8
slmarshall1 <slmarshall1@uclan.ac.uk>
2017-02-27 14:49:13 +0000

    my own sample data

---
+++ jason_example_1.js
@@ -0,0 +1,26 @@
+[
+  {
+    "name": "Gerry",
+    "species" : "cat",
+    "foods": {
+      "likes": ["tuna", "rabbit"],
+      "dislikes": ["onions"]
+    }
+  },
+  {
+    "name": "Daniel",
+    "species" : "dog",
+    "foods": {
+      "likes": ["bones", "biscuits"],
+      "dislikes": ["bread"]
+    }
+  },
+  {
+    "name": "Joey",
+    "species" : "pony",
+    "foods": {
+      "likes": ["polos"],
+      "dislikes": ["hay"]
+    }
+  }
+]
