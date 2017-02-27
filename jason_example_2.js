b33403818d00f09e428aae67bd7c4f2121eb5615
slmarshall1 <slmarshall1@uclan.ac.uk>
2017-02-27 16:02:36 +0000

    Add files via upload

---
+++ jason_example_2.js
@@ -0,0 +1,26 @@
+[
+  {
+    "name": "Indiana",
+    "species" : "horse",
+    "foods": {
+      "likes": ["polos", "hay"],
+      "dislikes": ["quavers"]
+    }
+  },
+  {
+    "name": "jip",
+    "species" : "dog",
+    "foods": {
+      "likes": ["ginger biscuits", "bones"],
+      "dislikes": ["dog food"]
+    }
+  },
+  {
+    "name": "Billy",
+    "species" : "fish",
+    "foods": {
+      "likes": ["fish food"],
+      "dislikes": ["nothing"]
+    }
+  }
+]
